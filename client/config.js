cbd50eb390d9e54909a4b2472e676bbf6b8f1128
Kai T. Ohlhus <k.ohlhus@gmail.com>
2021-10-27 13:23:27 +0900

    client/config.js: update default categories

--- client/config.js
+++ client/config.js
@@ -6,8 +6,11 @@ localStorage.setItem("defaultQuery",
                      "Action=get&OpenClosed=open&TrackerID=bugs&OrderBy=TrackerID,!ItemID&Limit=5&Format=HTMLCSS");
 
 var defaultQueries = [
-  {label: 'Octave 6',
-   api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=bugs&Category!=Forge,website&Release=6',
+  {label: 'Most recent items',
+   api:   'Action=get&Format=HTMLCSS&OrderBy=!LastComment&Limit=50',
+   url:   'https://savannah.gnu.org/bugs/?group=octave'},
+  {label: 'Octave 7',
+   api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=bugs&Category!=Forge,website&Release=7',
    url:   'https://savannah.gnu.org/bugs/index.php?go_report=Apply&group=octave&func=browse&set=custom&msort=0&report_id=221&advsrch=1&resolution_id%5B%5D=0&bug_group_id%5B%5D=0&status_id%5B%5D=1&priority%5B%5D=0&severity%5B%5D=0&category_id%5B%5D=100&category_id%5B%5D=110&category_id%5B%5D=101&category_id%5B%5D=102&category_id%5B%5D=104&category_id%5B%5D=105&category_id%5B%5D=106&category_id%5B%5D=107&category_id%5B%5D=103&category_id%5B%5D=114&category_id%5B%5D=112&category_id%5B%5D=109&category_id%5B%5D=113&release_id%5B%5D=173&release_id%5B%5D=174&release_id%5B%5D=176&platform_version_id%5B%5D=0&history_search=0&history_field=0&history_event=modified&history_date_dayfd=2&history_date_monthfd=8&history_date_yearfd=2021&chunksz=100&spamscore=5&boxoptionwanted=1#options'},
   {label: 'Severity >= 4',
    api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=bugs&Category!=Forge,website&Severity=4,5,6&Status!=Wont',
@@ -33,15 +36,9 @@ var defaultQueries = [
   {label: 'Ready for test',
    api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=bugs&Category!=Forge,website&Status=Ready',
    url:   'https://savannah.gnu.org/bugs/index.php?go_report=Apply&group=octave&func=browse&set=custom&msort=0&report_id=101&advsrch=1&status_id%5B%5D=1&resolution_id%5B%5D=10&submitted_by%5B%5D=0&assigned_to%5B%5D=0&category_id%5B%5D=100&category_id%5B%5D=110&category_id%5B%5D=101&category_id%5B%5D=102&category_id%5B%5D=104&category_id%5B%5D=105&category_id%5B%5D=106&category_id%5B%5D=107&category_id%5B%5D=103&category_id%5B%5D=114&category_id%5B%5D=112&category_id%5B%5D=109&bug_group_id%5B%5D=0&severity%5B%5D=0&priority%5B%5D=0&summary=&details=&sumORdet=0&history_search=0&history_field=0&history_event=modified&history_date_dayfd=10&history_date_monthfd=12&history_date_yearfd=2019&chunksz=100&spamscore=5&boxoptionwanted=1#options'},
-  {label: 'Bugs not fixed for the next Octave release',
-   api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=bugs&Category!=Forge,website&Status=Postponed',
-   url:   'https://savannah.gnu.org/bugs/index.php?go_report=Apply&group=octave&func=browse&set=custom&msort=0&report_id=101&advsrch=1&status_id%5B%5D=1&resolution_id%5B%5D=4&submitted_by%5B%5D=0&assigned_to%5B%5D=0&category_id%5B%5D=100&category_id%5B%5D=110&category_id%5B%5D=101&category_id%5B%5D=102&category_id%5B%5D=104&category_id%5B%5D=105&category_id%5B%5D=106&category_id%5B%5D=107&category_id%5B%5D=103&category_id%5B%5D=114&category_id%5B%5D=112&category_id%5B%5D=109&bug_group_id%5B%5D=0&severity%5B%5D=0&priority%5B%5D=0&summary=&details=&sumORdet=0&history_search=0&history_field=0&history_event=modified&history_date_dayfd=26&history_date_monthfd=10&history_date_yearfd=2020&chunksz=100&spamscore=5&boxoptionwanted=1#options'},
-  {label: 'Patch tracker patches submitted',
+  {label: 'Patch tracker patches',
    api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=patch&Category!=Forge,website&Status=None,Ready,Progress,Info,Bug',
    url:   'https://savannah.gnu.org/patch/index.php?go_report=Apply&group=octave&func=browse&set=custom&msort=0&report_id=101&advsrch=1&status_id%5B%5D=1&resolution_id%5B%5D=100&resolution_id%5B%5D=10&resolution_id%5B%5D=9&resolution_id%5B%5D=8&resolution_id%5B%5D=101&assigned_to%5B%5D=0&category_id%5B%5D=100&category_id%5B%5D=101&category_id%5B%5D=102&category_id%5B%5D=103&priority%5B%5D=0&summary=&details=&sumORdet=0&history_search=0&history_field=0&history_event=modified&history_date_dayfd=18&history_date_monthfd=1&history_date_yearfd=2020&chunksz=100&spamscore=5&boxoptionwanted=1#options'},
-  {label: 'Patches not included for the next Octave release',
-   api:   'Action=get&Format=HTMLCSS&OpenClosed=open&TrackerID=patch&Category!=Forge,website&Status=Postponed',
-   url:   'https://savannah.gnu.org/patch/index.php?go_report=Apply&group=octave&func=browse&set=custom&msort=0&report_id=101&advsrch=1&status_id%5B%5D=1&resolution_id%5B%5D=4&assigned_to%5B%5D=0&category_id%5B%5D=100&category_id%5B%5D=101&category_id%5B%5D=102&category_id%5B%5D=103&priority%5B%5D=0&summary=&details=&sumORdet=0&history_search=0&history_field=0&history_event=modified&history_date_dayfd=9&history_date_monthfd=2&history_date_yearfd=2020&chunksz=100&spamscore=5&boxoptionwanted=1#options'}
 ];
 
 localStorage.setItem("defaultQueries", JSON.stringify(defaultQueries));
